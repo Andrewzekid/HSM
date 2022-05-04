4f5ea0d1b7489c7896b2185b34803f73bd062553
Andrewzekid <79450923+Andrewzekid@users.noreply.github.com>
2022-05-04 17:30:07 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ from email.mime.text import MIMEText
 
 def access_website(wait_before_start=20):
     win32api.LoadKeyboardLayout('00000409',1) # to switch to english
-    x,y = pyautogui.locateCenterOnScreen("chrome.jpg",confidence=0.7)
+    x,y = pyautogui.locateCenterOnScreen("images/chrome.jpg",confidence=0.7)
     pyautogui.moveTo(x,y)
     pyautogui.click(x,y)
 
     time.sleep(1)
 
-    x2,y2 = pyautogui.locateCenterOnScreen("chromeprofile.jpg",confidence=0.7)
+    x2,y2 = pyautogui.locateCenterOnScreen("images/chromeprofile.jpg",confidence=0.7)
     pyautogui.moveTo(x2,y2)
     pyautogui.click(x2,y2)
 
@@ -56,7 +56,7 @@ def zoomOut(optionspath,number_of_zooms):
     locate_and_click(optionspath,grayscale=True,duration=0.01,tween=None)
     time.sleep(0.25)
 
-    x2,y2 = pyautogui.locateCenterOnScreen("zoomout.jpg",confidence=0.7,grayscale=True)
+    x2,y2 = pyautogui.locateCenterOnScreen("images/zoomout.jpg",confidence=0.7,grayscale=True)
     for _ in range(number_of_zooms):
         pyautogui.click(x2,y2)
 
@@ -125,11 +125,11 @@ def login():
     locate_and_click("account.jpg",duration=0.01,tween=None)
 
     # #wait until the login button is located
-    x,y = wait_until_image_located_on_screen("login.jpg",duration=4)
+    x,y = wait_until_image_located_on_screen("images/login.jpg",duration=4)
     pyautogui.moveTo(x,y)
     pyautogui.click(x,y)
     
-    x2,y2 = wait_until_image_located_on_screen("logincross.jpg",duration=30)
+    x2,y2 = wait_until_image_located_on_screen("images/logincross.jpg",duration=30)
     pyautogui.click(x2,y2)
     time.sleep(.3)
 
@@ -149,43 +149,43 @@ def wait_until_image_located_on_screen(path,duration,confidence=0.7,grayscale=Fa
             return x,y
 
 def refresh(path):
-    x,y = wait_until_image_located_on_screen("refresh.jpg",duration=8)
+    x,y = wait_until_image_located_on_screen("images/refresh.jpg",duration=8)
     pyautogui.click(x,y)
     time.sleep(.3)
 
 def make_payment(creditcardname,creditcardnumber,creditcardexpirydate,securitycode):
-    x4,y4 = wait_until_image_located_on_screen("continuepayment.jpg",duration=20)
+    x4,y4 = wait_until_image_located_on_screen("images/continuepayment.jpg",duration=20)
     pyautogui.click(x4,y4)
 
-    zoomOut("optionsdarkblue.jpg",number_of_zooms=4)
+    zoomOut("images/optionsdarkblue.jpg",number_of_zooms=4)
 
-    x2,y2 = wait_until_image_located_on_screen("continue.jpg",duration=20)
+    x2,y2 = wait_until_image_located_on_screen("images/continue.jpg",duration=20)
     pyautogui.click(x2,y2)
     
-    x3,y3 = wait_until_image_located_on_screen("creditcardbutton.jpg",confidence=0.95,duration=10)
+    x3,y3 = wait_until_image_located_on_screen("images/creditcardbutton.jpg",confidence=0.95,duration=10)
     pyautogui.click(x3,y3)
     
     #pause for a bit to allow the full form to load
     time.sleep(0.75)
 
     #click the input button that allows you to enter your name
-    locate_and_click("creditcardname.jpg",duration=0.01,tween=None)
+    locate_and_click("images/creditcardname.jpg",duration=0.01,tween=None)
     pyautogui.typewrite(creditcardname)
 
     #click the input button that allows you to enter your credit card number
-    locate_and_click("creditcardnumber.jpg",duration=0.01,tween=None)
+    locate_and_click("images/creditcardnumber.jpg",duration=0.01,tween=None)
     pyautogui.typewrite(creditcardnumber)
 
     #click the input button that allows you to enter the expiry date
-    locate_and_click("expirydate.jpg",duration=0.01,tween=None)
+    locate_and_click("images/expirydate.jpg",duration=0.01,tween=None)
     pyautogui.typewrite(creditcardexpirydate)
     
     #click the input button that allows you to enter your CV number
-    locate_and_click("securitycode.jpg",duration=0.01,tween=None)
+    locate_and_click("images/securitycode.jpg",duration=0.01,tween=None)
     pyautogui.typewrite(securitycode)
     
     #agree to the terms and conditions
-    locate_and_click("agreetoterms.jpg",duration=0.01,tween=None,confidence=0.95)
+    locate_and_click("images/agreetoterms.jpg",duration=0.01,tween=None,confidence=0.95)
 
     #continue with payment
     # locate_and_click("pay.jpg")
@@ -203,26 +203,26 @@ if __name__=="__main__":
     #attempt to locate targeted bag
     while not(keyboard.is_pressed("esc")):
         try:
-            locate_and_click("keyword.jpg",grayscale=True)
+            locate_and_click("images/keyword.jpg",grayscale=True)
             print("Success!")
         except:
             #refresh the page
-            refresh("refresh.jpg")
+            refresh("images/refresh.jpg")
         else:
             #targeted bag has been found
             print("Target identified!")
-            locate_and_click("keyword.jpg",grayscale=True)
+            locate_and_click("images/keyword.jpg",grayscale=True)
             time.sleep(1)
             try:
-                locate_and_click("cross.png",grayscale=True)
+                locate_and_click("images/cross.png",grayscale=True)
                 time.sleep(0.2)
             except:
                 pass
 
-            locate_and_click("shopping_cart.jpg",grayscale=True)
+            locate_and_click("images/shopping_cart.jpg",grayscale=True)
             
             #wait until the product has been added to the shopping cart
-            x1,y1 = wait_until_image_located_on_screen("viewshoppingbag.JPG",duration=20,grayscale=True)
+            x1,y1 = wait_until_image_located_on_screen("images/viewshoppingbag.JPG",duration=20,grayscale=True)
             pyautogui.click(x1,y1)
 
             make_payment("hello world","1234","05/2022","234")
